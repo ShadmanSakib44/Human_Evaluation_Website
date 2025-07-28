3798a861247b3d93e058c3c93e9e9fc93e416fc0
ShadmanSakib44 <shadmansakib20@iut-dhaka.edu>
2025-07-28 11:41:28 +0600

    undo

--- user-story-evaluator/next.config.ts
+++ user-story-evaluator/next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    esmExternals: 'loose'
-  },
-  // Disable static optimization for pages that use browser APIs
-  exportPathMap: function() {
-    return {
-      '/': { page: '/' },
-      // Don't pre-render /evaluate since it uses browser APIs
-    }
-  }
+  /* config options here */
 };
 
 export default nextConfig;
