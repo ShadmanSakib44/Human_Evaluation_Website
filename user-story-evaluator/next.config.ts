2b1f1401d99b946f01a6d7a8522e36e624296884
ShadmanSakib44 <shadmansakib20@iut-dhaka.edu>
2025-07-28 11:38:45 +0600

    config fix

--- user-story-evaluator/next.config.ts
+++ user-story-evaluator/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    esmExternals: 'loose'
+  },
+  // Disable static optimization for pages that use browser APIs
+  exportPathMap: function() {
+    return {
+      '/': { page: '/' },
+      // Don't pre-render /evaluate since it uses browser APIs
+    }
+  }
 };
 
 export default nextConfig;
